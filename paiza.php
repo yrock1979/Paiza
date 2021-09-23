4e4106eca2df0a6727ef248c9aabc9be01f59e3c
yrock1979 <88130247+yrock1979@users.noreply.github.com>
2021-09-23 22:56:59 +0900

    変数を変更しました

--- paiza.php
+++ paiza.php
@@ -1,6 +1,6 @@
 <?php
     // Dランク早解きセット　鉛筆の濃さ
-    $input = rtrim(fgets(STDIN));
-    $pen = ["" , "6B", "5B", "4B", "3B", "2B", "B", "HB", "F", "H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H"];
-    echo $pen[$input];
+    $pen = rtrim(fgets(STDIN));
+    $pens = ["" , "6B", "5B", "4B", "3B", "2B", "B", "HB", "F", "H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H"];
+    echo $pens[$pen];
 ?>
